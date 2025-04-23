16f6339dbf33315aeb1b93505865d7b62fcfb277
Luc Fery <Lucasus822@gmail.com>
2025-04-23 20:27:30 +0200

    JOIN des authors

--- app/models/photosModel.php
+++ app/models/photosModel.php
@@ -3,7 +3,11 @@ namespace App\Models\PhotosModel;
 use \PDO;
 
 function findAll(PDO $connexion):array {
-    $sql = "SELECT * FROM photos ORDER BY created_at DESC LIMIT 3;";
+    $sql = "SELECT *, p.id AS photoId, a.id AS authorId
+            FROM photos p 
+            JOIN authors a ON p.author_id = a.id 
+            ORDER BY p.created_at DESC 
+            LIMIT 3;";
     $rs = $connexion->query($sql);
     $photos = $rs->fetchAll(PDO::FETCH_ASSOC);
     return $photos;
